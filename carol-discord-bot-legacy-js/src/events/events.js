00299703cee97693a987a02abf07e4e7303469e6
Hades <marcelli335gamer@gmail.com>
2025-04-02 15:16:05 -0300

    removed legacy versions

--- carol-discord-bot-legacy-js/src/events/events.js
+++
@@ -1,8 +0,0 @@
-// * not used for now...
-class Events {
-  static ready = "ready";
-  static messageCreate = "messageCreate";
-  static interactionCreate = "interactionCreate";
-}
-
-module.exports = Events;
--- carol-discord-bot-legacy-py/src/commands/experience.py
+++
@@ -1,7 +0,0 @@
-import discord
-from discord.ext import commands
-
-
-class Experience(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
