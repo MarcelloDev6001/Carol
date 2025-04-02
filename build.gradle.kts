3eb29acd853a224c2feff54a029973955569cd86
Hades <marcelli335gamer@gmail.com>
2025-04-02 17:13:31 -0300

    shadow build added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,8 @@
-import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
-
 plugins {
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm") version "1.9.22"
     application
-    kotlin("plugin.serialization") version "2.1.0"
+    kotlin("plugin.serialization") version "1.9.22"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "com.hades.discord.bot.carol"
@@ -11,30 +10,35 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://jitpack.io") }
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
-    implementation("net.dv8tion:JDA:5.0.0-beta.10")
+    implementation("net.dv8tion:JDA:5.0.0-beta.20")
     implementation("club.minnced:jda-ktx:0.12.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.0")
-    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 
-    implementation("io.ktor:ktor-client-okhttp:2.3.11") // Versão estável
-
-    // Mantenha estas outras dependências
-    implementation("io.ktor:ktor-client-core:2.3.11")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.11")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.11")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("io.ktor:ktor-client-core:2.3.7")
+    implementation("io.ktor:ktor-client-okhttp:2.3.7")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
+tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    archiveFileName.set("CarolBot.jar")
+    mergeServiceFiles()
+}
+
 application {
-    mainClass = "com.hades.discord.bot.carol.MainKt"  // Definindo a classe principal corretamente
+    mainClass.set("MainKt")
 }
