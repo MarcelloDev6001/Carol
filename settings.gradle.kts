ec9f61815fa851956cc5bb13ff3828d68b65bb72
Hades <marcelli335gamer@gmail.com>
2025-04-02 15:40:23 -0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 rootProject.name = "carol-discord-bot"
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            library("deviousjda", "com.github.LorittaBot", "DeviousJDA").version("4153f3a5df")
-        }
-    }
-}
